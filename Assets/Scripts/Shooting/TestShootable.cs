179ab54c6d9ee66a54058cb4500a9d2fa76f9a52
Jinyang-H <jinyang961115@gmail.com>
2025-06-04 14:41:40 -0700

    Shooting_Sound

--- Assets/Scripts/Shooting/TestShootable.cs
+++ Assets/Scripts/Shooting/TestShootable.cs
@@ -2,8 +2,38 @@ using UnityEngine;
 
 public class TestShootable : MonoBehaviour, IShootable
 {
+    public GameObject hitEffectPrefab;
+    public AudioClip hitSound;
+    private AudioSource audioSource;
+
+    private float lastHitTime = -Mathf.Infinity;
+    private float soundCooldown = 0.3f;  
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnHit()
     {
-        Debug.Log("The object was shot");
+        float currentTime = Time.time;
+
+ 
+        if (currentTime - lastHitTime >= soundCooldown)
+        {
+            Debug.Log("The object was shot");
+
+            if (hitEffectPrefab)
+            {
+                Instantiate(hitEffectPrefab, transform.position + Vector3.up * 0.1f, Quaternion.identity);
+            }
+
+            if (hitSound && audioSource)
+            {
+                audioSource.PlayOneShot(hitSound);
+            }
+
+            lastHitTime = currentTime;
+        }
     }
 }
